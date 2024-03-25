4797c2ea0a8f219e9a81256feef11482fc902213
SahithiAnnem <sahithiannem21@gmail.com>
2024-03-24 23:47:09 -0500

    8

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
 import NavBar from './components/navbar';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import HomeScreen from './screens/HomeScreen';
+
+
 
 function App() {
   return (
     <div className="App">
       <NavBar/>
+      <Router>
+      <Routes>
+        <Route path="/home" element={<HomeScreen />} />
+      </Routes>
+    </Router>
+
+      
     </div>
   );
 }
