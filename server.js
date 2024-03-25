14cad068a7fdb9591e276303a4e133bbc87d6e14
SahithiAnnem <sahithiannem21@gmail.com>
2024-03-24 23:45:51 -0500

    homescreen

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const express= require("express");
 const app=express();
 const dbConfig = require('./db')
 
+const roomsRoute = require('.\routes\roomsRoute.js')
+
+app.use('/api/rooms', roomsRoute)
+
 const port = process.env.port || 5000;
 app.listen(port, () => console.log(`server running on port ${port}`));
 
